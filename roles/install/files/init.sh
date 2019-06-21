f52277f42b154210ef2367d41d58c04f83c9c303
Mozlk <xu_junkai@foxmail.com>
2019-06-21 15:41:35 +0800

    123

--- roles/install/files/init.sh
+++ roles/install/files/init.sh
@@ -10,6 +10,7 @@ echo -e 'MySQL username:root\nMySQL Password:'$new_password  > /credentials/pass
  
 sed -i "s/123456/$new_password/" /data/wwwroot/reviewboard/conf/settings_local.py
 userdel -rf websoft9      
+rm -rf /var/db/sudo/lectured/websoft9                                          
 sed -i "s/\/root\/init.sh//" /etc/rc.local                                                      
 rm -rf /root/init.sh
 
